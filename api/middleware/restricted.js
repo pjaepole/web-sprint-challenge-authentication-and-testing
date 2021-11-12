e9fd94b44710b14b72046894975384b91f1fc83d
Jaehun Park <pjaehun4122@gmail.com>
2021-11-12 11:07:22 -0800

    can get jokes when only supplied with valid token

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,21 @@
+const jwt=require('jsonwebtoken')
+const {JWT_SECRET}=require('../secrets/index')
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if (!token) {
+    return next({ status: 401, message: 'token required'})
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+       next({
+        status: 401,
+        message: "token invalid"
+      })
+    } else{
+      req.decodedToken = decodedToken
+      next()
+    }
+  })
   /*
     IMPLEMENT
 
